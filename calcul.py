03a703670963092f7ecc691799aeed3a5364c098
tarike <tarikelaoufi@gmail.com>
2026-06-20 10:39:28 +0100

    calcule

---
+++ calcul.py
@@ -0,0 +1,12 @@
+
+# calcul.py
+def addition(a, b):
+    return a + b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Division par zéro interdite")
+    return a / b
